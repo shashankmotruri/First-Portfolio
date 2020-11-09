ae27d046987e629e79979f86d61a7bb112aa187a
shashankmotruri <motrurishashank000@gmail.com>
2020-11-09 12:36:11 +0530

    updated

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -119,3 +119,47 @@ document.querySelectorAll('ul li a').forEach((link) => {
   link.addEventListener('click', scrollTo);
 })
 
+
+
+//MySkills
+var html = document.querySelector('.html');
+var css = document.querySelector('.css');
+var sass = document.querySelector('.sass');
+var git = document.querySelector('.git');
+var bootstrap = document.querySelector('.bootstrap');
+var js = document.querySelector('.js');
+
+
+    var elems = [html, css, sass, git, bootstrap, js];
+
+    var counter = 0;
+
+    function removeClass () {
+
+        if (counter < elems.length) {
+            elems[counter].classList.remove('hidden');
+            counter++;
+            setTimeout(removeClass, 1000)
+        } else {
+           addClass();
+        }
+    }
+
+    function addClass () {
+      for(var i = 0; i < elems.length; i++) {
+        elems[i].classList.add('hidden');
+      }
+    }
+
+removeClass();
+
+setInterval(function(){
+  
+  counter = 0;
+  removeClass();
+  
+}, 6600)
+
+
+//Skill bar
+
